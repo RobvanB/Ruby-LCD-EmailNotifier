0bb5b2b7f6b5ef5a5a80d232ccdf52ece4524343
RobvanB <RobvanB@gmail.com>
2012-12-09 17:38:49 -0500

    Email config file added. Working now for multiple accounts. Added in the class for writing to the LCD as well

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Ruby-LCD-EmailNotifier
 
 Displays new email count (Gmail) to 16x2 LCD connected to RaspberryPi
 
+Rename the sample_gmail_count_config.xml to gmail_count_config.xml and put your
+ accounts and passwords in there.
+ Note, if you have an 'ampersand' in your password or username (&) - put it in the
+ file like this: "&amp;"
--- gmail_count.rb
+++ gmail_count.rb
@@ -1,13 +1,14 @@
 class GmailCount
   require 'nokogiri'
   require 'net/https'
+  require_relative('ruby-lcd.rb')
 
-  username = ""
-  password = ""
+  @lcd = RubyLcd.new()
 
-  begin
+  def self.getCount(username, password)
     http = Net::HTTP.new("mail.google.com", 443)
     http.use_ssl = true
+
     http.start do |http|
       req = Net::HTTP::Get.new("/mail/feed/atom", {"User-Agent" =>
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)"})
@@ -20,11 +21,30 @@ class GmailCount
 
     node = htmlDoc.xpath("//title")
     if (node[0].content == 'Unauthorized')
-      puts "Authentication Error"
+      #puts "Authentication Error for account " + username
+      @lcd.lcdPrint("Authentication Error for account " + username)
     else
-      node =  htmlDoc.xpath("//fullcount")
-      puts " The Node "
-      puts node[0].content
+      node      = htmlDoc.xpath("//fullcount")
+      mailCount = node[0].content
+      #puts username + " : " + node[0].content
+      shortName = username.split("@")[0]
+      puts shortName + " : " + mailCount
+      @lcd.lcdPrint(shortName + " : " + mailCount)
     end
   end
+
+  # Open the config file with the account info
+  cfg = File.open("gmail_count_config.xml")
+  doc = Nokogiri::XML(cfg)
+  cfg.close
+
+  doc.xpath("//account").each { |acc|
+    #puts acc.to_s
+    username = acc.xpath("uname")[0].content
+    password = acc.xpath("pw")[0].content
+    getCount(username, password)
+    sleep 2
+  }
+
+
 end
