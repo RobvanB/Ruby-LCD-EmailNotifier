827861117a94eb7d435b99e3d0216e6711731688
RobvanB <RobvanB@gmail.com>
2012-12-14 19:17:28 -0500

    Built in a loop so the display is updated every 2 seconds as it loops through all the accounts. The actual email check is only done every 5 mins

--- gmail_count.rb
+++ gmail_count.rb
@@ -4,6 +4,7 @@ class GmailCount
   require_relative('ruby-lcd.rb')
 
   @lcd = RubyLcd.new()
+  @countHash = Hash.new()
 
   def self.getCount(username, password)
     http = Net::HTTP.new("mail.google.com", 443)
@@ -29,20 +30,42 @@ class GmailCount
       #puts username + " : " + node[0].content
       shortName = username.split("@")[0]
       puts shortName + " : " + mailCount
-      @lcd.lcdPrint(shortName + " : " + mailCount)
+      #@lcd.lcdPrint(shortName + " : " + mailCount)
+      @countHash[shortName] = mailCount
     end
   end
 
+  def self.getCountLoop()
+    @countHash.clear
+    @doc.xpath("//account").each { |acc|
+      #puts acc.to_s
+      username = acc.xpath("uname")[0].content
+      password = acc.xpath("pw")[0].content
+      getCount(username, password)
+    }
+  end
+
+
+
   # Open the config file with the account info
   cfg = File.open("gmail_count_config.xml")
-  doc = Nokogiri::XML(cfg)
+  @doc = Nokogiri::XML(cfg)
   cfg.close
+  checkCount = 300
 
-  doc.xpath("//account").each { |acc|
-    #puts acc.to_s
-    username = acc.xpath("uname")[0].content
-    password = acc.xpath("pw")[0].content
-    getCount(username, password)
-    sleep 2
-  }
+  while true
+    # Keep updating the display, but only check for email once every 5 mins
+    if (checkCount == 300)
+        checkCount = 0
+        getCountLoop()
+    end
+
+    @countHash.each() { |uname, mailcount|
+      #puts 'lcd'
+      @lcd.lcdPrint(uname + " : " + mailcount)
+      sleep 2
+    }
+
+      checkCount += 2
+  end
 end
